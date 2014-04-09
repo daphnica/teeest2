c8f0a255564c4255aa34240556e752125815caae
daitm <tianming.dai@archermind.com>
2014-04-09 11:17:47 +0800

    no9

--- src/com/mymusicplayer/MusicPlayer.java
+++ src/com/mymusicplayer/MusicPlayer.java
@@ -69,7 +69,7 @@ public class MusicPlayer extends Activity implements OnItemClickListener,
 						Toast.LENGTH_LONG).show();
 			}
 		}
-		/** git test,can you find me?yes,you can!no8 **/
+		/** git test,can you find me?yes,you can!no9 **/
 
 		entity = CallbackDeleteEntity.getInstance();
 		entity.setMethodCallback(this);
